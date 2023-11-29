bf3ac11264a1ebe116ca171b3131949aa1d9f3ae
1637908 <91459769+1637908@users.noreply.github.com>
2023-11-29 14:40:52 +0100

    Add providers.tf

---
+++ providers.tf
@@ -0,0 +1,13 @@
+terraform {
+	required_providers {
+		opennebula = {
+		source = "OpenNebula/opennebula"
+		version = "~> 1.3"
+		}
+	}
+}
+provider "opennebula" {
+	endpoint = "http://nebulacaos.uab.cat:2633/RPC2"
+	username = "gixpd-29"
+	password = "S5agov4Gjczz"
+}
